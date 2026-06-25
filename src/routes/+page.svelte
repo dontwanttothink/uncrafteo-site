09172e97e1c3ffc36a658cc232dc844bbcf9d552
dontwanttothink <git@dontwanttoth.ink>
2026-06-24 21:05:14 -0500

    Add all the content

---
+++ src/routes/+page.svelte
@@ -0,0 +1,18 @@
+<main>
+	<h1>UNcrafteo</h1>
+	<p class="subtitle">juega.uncrafteo.net</p>
+
+	<p>Para jugar en nuestro servidor de Minecraft, <a href="https://discord.gg/kCMbz7zhuG">únete a la comunidad en Discord</a>.</p>
+
+	<h2>Reglas</h2>
+	<ol>
+		<li><a href="https://www.lasillavacia.com/red-de-expertos/red-de-derecho-constitucional/de-la-espriella-sin-etica-sin-derecho-y-sin-sentido-de-civilidad/">Abelardo</a> no puede entrar a UNcrafteo porque está vetado.</li>
+		<li>Están mal la misoginia, la homofobia, la transfobia, el racismo y cualquier otro prejuicio cruel.</li>
+		<li>El truco de desconectarse para no morir está inminentemente prohibido; si se usa este hack, el jugador será puesto en clear inventory. Queremos juego transparente y justo para todos.</li>
+	</ol>
+</main>
+
+<footer>
+	<p style:opacity=0.7>Con amor, de snati y nicobbvlogs</p>
+	<p style:opacity=0.9>🐮🦧🌻</p>
+</footer>
