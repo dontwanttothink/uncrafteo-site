b80512ebc80045853b440570442fcdcdc59b939c
dontwanttothink <git@dontwanttoth.ink>
2026-07-20 16:18:11 -0500

    Intentar agregar una página 404 correcta

---
+++ src/routes/+error.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import { page } from "$app/state";
+</script>
+
+<main>
+	<h1>Oops.</h1>
+	<p>
+		Se produjo un error {page.status}. En inglés, ese código significa “{page
+			.error?.message ?? "❓"}”.
+	</p>
+	<p><a href="/">Regresa al inicio 🧚.</a></p>
+</main>
+
+<style>
+	main {
+		text-align: center;
+	}
+</style>
