b96feb248a7d5a03f7b9a495867d5e306955ee95
dontwanttothink <git@dontwanttoth.ink>
2026-07-20 16:54:04 -0500

    Clarificar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import favicon from '$lib/assets/favicon.jpeg';
+	import favicon from "$lib/assets/favicon.jpeg";
 	import "../app.css";
 
 	let { children } = $props();
@@ -8,6 +8,10 @@
 <svelte:head>
 	<link rel="icon" href={favicon} />
 	<title>UNcrafteo</title>
+	<meta
+		name="description"
+		content="De estudiantes de la UNAL, un servidor de Minecraft con mods."
+	/>
 </svelte:head>
 
 {@render children()}
