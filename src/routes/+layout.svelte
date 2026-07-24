64e05ee2bdf0575cd4983f39076789e6cbc5552a
dontwanttothink <git@dontwanttoth.ink>
2026-07-24 15:33:01 -0500

    Improve layout shift and set up formatting

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,12 +6,12 @@
 </script>
 
 <svelte:head>
-	<link rel="icon" href={favicon} />
+	<link rel="icon" href={favicon}>
 	<title>UNcrafteo</title>
 	<meta
 		name="description"
 		content="De estudiantes de la UNAL, un servidor de Minecraft con mods."
-	/>
+	>
 </svelte:head>
 
 {@render children()}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,11 +3,10 @@
 	<p class="subtitle">juega.uncrafteo.net</p>
 
 	<p>
-		Un servidor de Minecraft con mods de parte de estudiantes de la
-		Universidad Nacional de Colombia, sede Bogotá. Para jugar en nuestro
-		servidor de Minecraft, <a href="https://discord.gg/kCMbz7zhuG"
-			>únete a la comunidad en Discord</a
-		>.
+		Un servidor de Minecraft con mods de parte de estudiantes de la Universidad
+		Nacional de Colombia, sede Bogotá. Para jugar en nuestro servidor de
+		Minecraft,
+		<a href="https://discord.gg/kCMbz7zhuG">únete a la comunidad en Discord</a>.
 	</p>
 
 	<p style:opacity="0.6" style:margin-bottom="1rem">
@@ -20,16 +19,17 @@
 			<a
 				href="https://www.lasillavacia.com/red-de-expertos/red-de-derecho-constitucional/de-la-espriella-sin-etica-sin-derecho-y-sin-sentido-de-civilidad/"
 				>Abelardo</a
-			> no puede entrar a UNcrafteo porque está vetado.
+			>
+			no puede entrar a UNcrafteo porque está vetado.
 		</li>
 		<li>
 			Están mal la misoginia, la homofobia, la transfobia, el racismo y
 			cualquier otro prejuicio cruel.
 		</li>
 		<li>
-			El truco de desconectarse para no morir está inminentemente
-			prohibido; si se usa este hack, el jugador será puesto en clear
-			inventory. Queremos juego transparente y justo para todos.
+			El truco de desconectarse para no morir está inminentemente prohibido; si
+			se usa este hack, el jugador será puesto en clear inventory. Queremos
+			juego transparente y justo para todos.
 		</li>
 	</ol>
 </main>
